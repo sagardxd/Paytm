1349dde4b7fab99fdd4884163692005143c255ac
Sagar Yadav <sagardxd5@gmail.com>
2024-04-12 14:51:04 +0530

    try catch

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,12 +3,19 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
-    const session = await getServerSession(authOptions);
-    if (session.user) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (session.user) {
+            return NextResponse.json({
+                user: session.user
+            })
+        }
+    } catch (error) {
         return NextResponse.json({
-            user: session.user
+            msg: "User not logged in"
         })
     }
+
     return NextResponse.json({
         message: "You are not logged in"
     }, {
