6ed350b9f61a00b07d2431ca6b57eb5b8cc2236d
Sagar Yadav <sagardxd5@gmail.com>
2024-04-17 12:59:48 +0530

    added zod validation

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,52 +1,52 @@
-import express from "express";
-import db from '@repo/db/client'
+    import express from "express";
+    import db from '@repo/db/client'
+    import { PaymentInformation } from "./zod/webhook";
 
-const app = express();
+    const app = express();
 
-app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
-    const paymentInformation = {
-        token: req.body.token,
-        userId: req.body.user_identifier,
-        amount: req.body.amount
-    };
+    app.post("/hdfcWebhook", async (req, res) => {
+        //TODO: Add zod validation here?
+        const result = PaymentInformation.safeParse(req.body);
+        if (!(result.success)) {
+            return res.status(411).json({ msg: "Input errors", error: result.error });
+        }
 
-    try {
-        await db.$transaction([
+        try {
+            await db.$transaction([
 
-            // updating the balance in db
-            db.balance.update({
-                where: {
-                    userId: paymentInformation.userId
-                },
-                data: {
-                    amount: {
-                        increment: paymentInformation.amount
+                // updating the balance in db
+                db.balance.update({
+                    where: {
+                        userId: result.data.userId
+                    },
+                    data: {
+                        amount: {
+                            increment: result.data.amount
+                        }
                     }
-                }
-            }),
+                }),
 
-            // adding it in the onramptransaction
-            db.onRampTransaction.update({
-                where: {
-                    token: paymentInformation.token
-                },
-                data: {
-                    status: "Sucess"
-                }
+                // adding it in the onramptransaction
+                db.onRampTransaction.update({
+                    where: {
+                        token: result.data.token
+                    },
+                    data: {
+                        status: "Sucess"
+                    }
+                })
+            ])
+
+            res.json({
+                message: "Captured"
             })
-        ])
 
-        res.json({
-            message: "Captured"
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(411).json({
-            message: "Error while processing data"
-        })
-    }
+        } catch (error) {
+            console.log(error);
+            res.status(411).json({
+                message: "Error while processing data"
+            })
+        }
 
 
-})
+    })
